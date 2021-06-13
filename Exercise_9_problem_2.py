3a6fb1be0e09d5f45ab92a1795004b664f78a080
mtiseagugis2021 <81938125+mt-ise-agugis2021@users.noreply.github.com>
2021-06-13 15:35:45 +0000

    I created a Shapely point .

--- Exercise_9_problem_2.py
+++ Exercise_9_problem_2.py
@@ -11,7 +11,9 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 # YOUR CODE HERE 1 to read data
-data = None
+data = pd.read_csv('data/some_posts.csv')
+point =lambda row:Point(row['lat'], row['lon'])
+data['geometry'] = data.apply(point, axis=1)
 
 
 # CODE FOR TESTING YOUR SOLUTION
